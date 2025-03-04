8372a81deceeb27e8ddc9b54ecb5148fb84ec518
sand4570 <s224323@student.dtu.dk>
2025-03-04 20:20:39 +0100

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Sandie Petersen on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
