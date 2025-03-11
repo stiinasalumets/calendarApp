77f693f02378623b721cc94e8988b72ccb0fa0a0
stiinasalumets <stiina.salumets@gmail.com>
2025-03-11 11:29:44 +0100

    calendar

--- HabitTracker/CalendarView.swift
+++ HabitTracker/CalendarView.swift
@@ -2,17 +2,7 @@
 //  CalendarView.swift
 //  HabitTracker
 //
-//  Created by Stiina Salumets on 04.03.2025.
+//  Created by Stiina Salumets on 11.03.2025.
 //
 
-import SwiftUI
-
-struct CalendarView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    CalendarView()
-}
+import Foundation
