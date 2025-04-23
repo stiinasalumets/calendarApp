655800e34bf08befeaf0e1bf16bbf492374bf122
Sandie <s224323@student.dtu.dk>
2025-04-23 09:49:17 +0200

    Add completion stats and framework for streak

---
+++ HabitTracker/StatisticsView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+import CoreData
+
+struct StatisticsView: View {
+    @Environment(\.managedObjectContext) private var moc
+    @State private var viewModel: ViewModel
+    
+    
+    
+    init(moc: NSManagedObjectContext) {
+        self._viewModel = State(wrappedValue: ViewModel(moc: moc))
+    }
+    
+    var body: some View {
+        VStack{
+            VStack {
+                Text("Statistics")
+                    .font(.largeTitle)
+                    .padding(.top)
+                
+            }
+            
+            VStack(alignment: .center, spacing: 4) {
+                Text("Streak")
+                    .font(.title3)
+                    
+                Text("\(viewModel.habitStreak) days")
+                    .font(.title2)
+                    .bold()
+                    .foregroundStyle(.secondary)
+            }
+            .padding(.vertical, 8)
+            
+            List {
+                ForEach(viewModel.habitStats.sorted(by: { $0.key.title ?? "" < $1.key.title ?? "" }), id: \.key) { habit, percentage in
+                    HStack {
+                        Text(habit.title ?? "Unnamed Habit")
+                        Spacer()
+                        Text("\(percentage)%")
+                            .foregroundStyle(.secondary)
+                    }
+                }
+            }
+            
+        }
+    }
+}
